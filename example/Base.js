3efffd845e5c8f02da53201c8947cde51719bf25
Fritz Lin <uxfritz@163.com>
2017-06-04 19:40:47 +0800

    0.0.1

---
+++ example/Base.js
@@ -0,0 +1,20 @@
+let list = [
+  'mongo-model-2/src/SchemaBase',
+  'mongo-model-2/src/DbBase',
+  'mongo-model-2/src/CollBase',
+  'mongo-model-2/src/DateBase',
+  './AppBase'
+].map(require)
+
+// 将list循环继承 出一个最终的Base类
+let Base = list.reduce((B, fn, i) => {
+  let C = fn(B)
+  return C
+}, null)
+
+module.exports = Base
+
+Base.mongoConfig = {
+  port: process.env.MONGO_PORT,
+  dbName: process.env.MONGO_DBNAME
+}
